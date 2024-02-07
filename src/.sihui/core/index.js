920570bdb3361cec0e5949fc1473175bbf15fdfc
WSH <web_wangsihui@163.com>
2024-02-07 18:29:26 +0800

    chore(src): 添加一个路由处理函数

--- src/.sihui/core/index.js
+++ src/.sihui/core/index.js
@@ -1,9 +1,31 @@
 
+import routes from '@/config/routes';
+import { useNavigate } from 'react-router-dom';
+
 const s_createBrowserRouter = (routes) => {
     // 建立路径和名字映射
     const s_core_route_map = new Map();
 
-    // (function loopRoute())
+    (function loopRoute(routes) {
+        routes?.forEach(item => {
+            if (item.name) s_core_route_map.set([item.name], [item.path]);
+            if (item?.children?.length !== 0) loopRoute(item.children)
+        })
+        return;
+    })(routes)
+
+    return s_core_route_map;
 }
 
+const routeMap = s_createBrowserRouter(routes);
+
+const sNavigate = (name) => {
+    const path = routeMap.get([name]);
+    const navigate = useNavigate();
+    navigate(path);
+}
+
+
 export { RouterProvider, createBrowserRouter, Link, useNavigate, Outlet } from 'react-router-dom';
+
+export { s_createBrowserRouter, sNavigate };
--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,9 +1,18 @@
 import React from "react";
-import { RouterProvider, createBrowserRouter, Link } from "@sihui";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  Link,
+  s_createBrowserRouter,
+} from "@sihui";
 import routes from "@/config/routes";
 
 const router = createBrowserRouter(routes);
 
+console.log("s_createBrowserRouter", s_createBrowserRouter(routes));
+
 export default () => {
   return <RouterProvider router={router} />;
 };
+
+
