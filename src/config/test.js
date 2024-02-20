00b8e29030d0283e104f3cfe9024a0d7620d240c
WSH <web_wangsihui@163.com>
2024-02-20 14:54:36 +0800

    fix(src): 预处理调试补充

--- src/config/test.js
+++ src/config/test.js
@@ -6,6 +6,9 @@ const result = str.toString();
 
 const reg1 = /component.*,/g;
 
+
+// 这里其实可以匹配 { },然后用 ， 和 ： 把数据都分离出来，然后决定最终生成文件的样子
+
 const newStr = result.replace(reg1, (str) => {
     const arr = str.split(':');
     arr[1] = `()=>import(${arr[1]}),`;
